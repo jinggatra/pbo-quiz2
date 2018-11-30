d98330dfa537f020894fde398c516eddc4fae189
J I N G G A <J I N G G A@DESKTOP-FVRNFBC>
2018-11-30 22:06:03 +0700

    Penambahan Class Item.java

--- test/com/taufik/kuis2/Item.java
+++ test/com/taufik/kuis2/Item.java
@@ -1,42 +1,43 @@
+package com.taufik.kuis2;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.taufik.kuis2;
 
 /**
  *
  * @author J I N G G A
  */
 public class Item {
-    private String nama; //Variable nama untuk menyimpan nama pada barang
-    private double harga; //Variavle harga untuk menyimpan nilai harga pada barang
-    private int jumlah; //variable jumlah untuk menyimpan jumlah pada barang
+    private String nama;
+    private int harga;
+    private int jumlah;
 
-    //Konstruktor dengan 2 parameter
     public Item(String nama, int jumlah) {
         this.nama = nama;
         this.jumlah = jumlah;
     }
-    
-    //Konstruktor dengan 3 parameter
-    public Item(String nama, int jumlah, double harga) {
+
+    public Item(String nama, int harga, int jumlah) {
         this.nama = nama;
         this.harga = harga;
         this.jumlah = jumlah;
-        
-        switch(nama){
+        switch (nama) {
             case "Kopi" : this.harga = 10000;
             break;
             case "Gula" : this.harga = 30000;
             break;
-            case "Susu" : this.harga = 15000;
+            case "Susu" : this.harga = 20000;
             break;
         }
     }
-    
-    //Setter dan Getter
+
+    public Item(String nama, int jumlah, int[] harga) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public String getNama() {
         return nama;
     }
@@ -45,11 +46,11 @@ public class Item {
         this.nama = nama;
     }
 
-    public double getHarga() {
+    public float getHarga() {
         return harga;
     }
 
-    public void setHarga(double harga) {
+    public void setHarga(int harga) {
         this.harga = harga;
     }
 
@@ -61,13 +62,10 @@ public class Item {
         this.jumlah = jumlah;
     }
     
-    public double getTotal(){
+    public float getTotal(){
         return this.harga * this.jumlah;
     }
-
-    public String toString() {
+    public String toString(){
         return this.nama;
     }
 }
-
-
